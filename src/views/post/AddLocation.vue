3b87501f93722e4ee8826199ff092d57f3f638d9
CuberLuo <1501858187@qq.com>
2023-12-16 16:00:47 +0800

    添加地点用户体验优化

--- src/views/post/AddLocation.vue
+++ src/views/post/AddLocation.vue
@@ -7,8 +7,17 @@
     </van-cell>
     <van-action-sheet v-model:show="show" title="添加地点">
       <div class="content">
+        <div>
+          <span>当前地点:{{ cellValue }}</span>
+        </div>
         <form action="/">
-          <van-search v-model="searchValue" placeholder="请输入地点关键词" @search="onSearch" />
+          <van-search
+            id="placeInput"
+            v-model="searchValue"
+            placeholder="请输入地点关键词"
+            @search="onSearch"
+            @update:model-value="onSearch"
+          />
         </form>
         <van-cell-group>
           <van-cell
@@ -28,12 +37,51 @@
 <script setup>
 import AMapLoader from '@amap/amap-jsapi-loader'
 import amap from '@/components/data/amap.json'
+
+let userCity = ''
+onMounted(() => {
+  AMapLoader.load({
+    key: amap.key,
+    version: '2.0',
+    plugins: ['AMap.Geolocation', 'AMap.Geocoder']
+  }).then((AMap) => {
+    const geolocation = new AMap.Geolocation({
+      useNative: true,
+      timeout: 10 * 1000,
+      enableHighAccuracy: true //使用高精度定位
+    })
+    geolocation.getCurrentPosition(function (status, result) {
+      if (status == 'complete') {
+        console.log('定位成功', result)
+        const { lat, lng } = result.position
+        //获取经纬度逆地理编码为详细地址
+        const geocoder = new AMap.Geocoder()
+        const lnglat = [lng, lat]
+        geocoder.getAddress(lnglat, function (status, result) {
+          if (status === 'complete' && result.info === 'OK') {
+            // result为对应的地理位置详细信息
+            console.log('地理位置信息:', result)
+            cellValue.value = result.regeocode.formattedAddress
+            onClickCell(cellValue.value, lng, lat)
+            userCity = result.regeocode.addressComponent.city
+            console.log(userCity)
+          }
+        })
+      } else {
+        console.log('定位失败', result)
+        showFailToast('无法获取当前定位')
+      }
+    })
+  })
+})
+
 const show = ref(false)
 const cellValue = ref('')
 const searchValue = ref('')
 const poiList = ref([])
 
 const onSearch = (val) => {
+  if (val == '') return
   showLoadingToast({
     message: '搜索中...',
     forbidClick: true,
@@ -45,8 +93,11 @@ const onSearch = (val) => {
     version: '2.0',
     plugins: ['AMap.PlaceSearch']
   }).then((AMap) => {
-    var keywords = val
-    var placeSearch = new AMap.PlaceSearch()
+    const keywords = val
+    const placeSearch = new AMap.PlaceSearch({
+      city: userCity
+    })
+
     placeSearch.search(keywords, function (status, result) {
       console.log(status)
       if (status == 'complete') {
