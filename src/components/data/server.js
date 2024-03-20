cd7e3cae96bef61cc180afb1a7b1077aa924c30a
CuberLuo <1501858187@qq.com>
2024-03-20 15:43:56 +0800

    添加环境配置文件

--- src/components/data/server.js
+++ src/components/data/server.js
@@ -1,5 +1,6 @@
 export const getServerUrl = () => {
-  const url = 'http://localhost:1029'
-  // const url="https://api.within-circle.techvip.site"
+  // const url = 'http://localhost:1029'
+  // const url = 'https://api.within-circle.techvip.site'
+  const url = import.meta.env.VITE_SOCKET_SERVER
   return url
 }
